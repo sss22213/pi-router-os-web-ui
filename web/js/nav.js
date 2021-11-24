eb1dfcf99a6c91d0715a400ad36c411871df6f65
Nian Bohung <n0404.n0404@gmail.com>
2021-11-24 07:53:10 -0800

    Web UI: Develop website aligment
    Signed-off-by: Nian Bohung <n0404.n0404@gmail.com>

---
+++ web/js/nav.js
@@ -0,0 +1,29 @@
+$(function() {
+    $("#dashboard").click(function() {
+        alert("dashboard");
+    });
+
+    $("#mode_switch").click(function() {
+        alert("mode_switch");
+    });
+
+    $("#wan").click(function() {
+        alert("wan");
+    });
+
+    $("#lan").click(function() {
+        alert("lan");
+    });
+
+    $("#firewall").click(function() {
+        alert("firewall");
+    });
+
+    $("#system_setting").click(function() {
+        alert("system_setting");
+    });
+
+    $("#network_tool").click(function() {
+        alert("network_tool");
+    });
+});
